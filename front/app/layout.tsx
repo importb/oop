5ba36159b2fb1480f335a9e860da0eb381a3bfd9
Kevin Akkermann <kevinakk2003@gmail.com>
2024-04-18 12:55:15 +0300

    frondi basic failid ja header

---
+++ front/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+import Header from "@/components/header";
+
+export const metadata: Metadata = {
+  title: "Programmeerimine II kaapija"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ front/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-between">
+      <div className="h-full w-full bg-white"></div>
+    </main>
+  );
+}
