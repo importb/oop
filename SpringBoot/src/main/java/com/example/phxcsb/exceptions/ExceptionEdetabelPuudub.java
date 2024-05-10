b4aed49110d85c46098570bd2c74653a24320d5f
byte <70361431+im-byte@users.noreply.github.com>
2024-05-10 22:40:12 +0300

    edetabel puudub
    edetabel puudub

---
+++ SpringBoot/src/main/java/com/example/phxcsb/exceptions/ExceptionEdetabelPuudub.java
@@ -0,0 +1,20 @@
+package com.example.phxcsb.exceptions;
+
+public class ExceptionEdetabelPuudub extends Exception {
+
+    public ExceptionEdetabelPuudub() {
+        super();
+    }
+
+    public ExceptionEdetabelPuudub(String message) {
+        super(message);
+    }
+
+    public ExceptionEdetabelPuudub(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExceptionEdetabelPuudub(Throwable cause) {
+        super(cause);
+    }
+}
